84b1b91c43426feaa89c43c2589a84f8e98ec84e
ismail036 <90846601+ismail036@users.noreply.github.com>
2022-02-07 19:31:08 +0300

    Add files via upload

---
+++ OrtalamaHesaplama.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Matematik notunuzu giriniz :  ");
+        int matematik = input.nextInt();
+
+        System.out.println("Fizik notunuzu giriniz :  ");
+        int fizik = input.nextInt();
+
+        System.out.println("Kimya notunuzu giriniz :  ");
+        int kimya = input.nextInt();
+
+        System.out.println("Türkçe notunuzu giriniz :  ");
+        int turkçe = input.nextInt();
+
+        System.out.println("Tarih notunuzu giriniz :  ");
+        int tarih = input.nextInt();
+
+        System.out.println("Müzik notunuzu giriniz :  ");
+        int muzik = input.nextInt();
+
+
+        int toplam = matematik + tarih + fizik + kimya + muzik + turkçe;
+        double notOrtalamsı = toplam / 6;
+
+        System.out.println("Not ortalamanız :  " + notOrtalamsı);
+
+    }
+}
