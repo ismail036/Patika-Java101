6b407703d99edb3546498cfeee06ddac79b16a91
ismail036 <90846601+ismail036@users.noreply.github.com>
2022-09-02 11:06:18 +0300

    Add files via upload

---
+++ Sayıları Büyükten Küçüğe Sıralama/src/App.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a , b,c;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("1. sayı : ");
+        a = input.nextInt();
+
+        System.out.println("2. sayı : ");
+        b = input.nextInt();
+
+        System.out.println("3. sayı : ");
+        c = input.nextInt();
+
+        if((a > b) && (a > c)){
+            if(b > c){
+                 System.out.println("a > b > c");
+            }else{
+                System.out.println("a > c > b");
+            }
+        }else if((b > a) && (b > c)){
+            if(a > c){
+                System.out.println("b > a > c");
+            }else{
+                System.out.println("b > c > a");
+            }
+        }else if((c > b) && (c > a)){
+            if(a > b){
+                System.out.println("c > b > a");
+            }else{
+                System.out.println("c > a > b");
+            }
+        }
+    }
+}
