dc371bdaa28514431526cf1c0d61c756f5aa7a8e
ismail036 <90846601+ismail036@users.noreply.github.com>
2022-09-07 11:46:12 +0300

    Delete Mayin Tarlası directory

--- Mayin Tarlası/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-       
-        MineSweeper game = new MineSweeper(   {{1,2},{1,2}}  );
-
-    }
-}
--- Mayin Tarlası/src/MineSweeper.java
+++
@@ -1,9 +0,0 @@
-public class MineSweeper {
-    int matris[][];
-
-    MineSweeper(int[][] matris){
-        this.matris = matris;
-    }
-
-
-}
